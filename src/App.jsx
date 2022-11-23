5f0e7bc2174ddfb7d4af0090929d5ec947f4893c
tahdi-islam <tshdi60@gmail.com>
2022-11-24 00:57:43 +0600

    create main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Router/router";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl font-bold text-center">Hello laptop mart</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 }
