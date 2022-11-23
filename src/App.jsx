7e0e28df865c20fcef103a6bf544232ce0d57963
tahdi-islam <tshdi60@gmail.com>
2022-11-24 00:45:15 +0600

    create laptop mart project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold text-center">Hello laptop mart</h1>
+    </div>
+  );
+}
+
+export default App;
