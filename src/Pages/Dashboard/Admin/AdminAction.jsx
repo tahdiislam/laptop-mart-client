521c75a84d7fb026d9e04b157b0a9579cf12f458
tahdi-islam <tshdi60@gmail.com>
2022-11-27 02:54:45 +0600

    post category with image

--- src/Pages/Dashboard/Admin/AdminAction.jsx
+++ src/Pages/Dashboard/Admin/AdminAction.jsx
@@ -1,11 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../../Context/AuthProvider";
 
 const AdminAction = () => {
+  const { logOut } = useContext(UserContext);
+  const navigate = useNavigate();
+  // image bb api key
+  const imageBbApiKey = import.meta.env.VITE_IMAGE_BB_API_KEY;
   // add category handler
   const handleAddCategory = (event) => {
     event.preventDefault();
-    console.log(event.target.category.value);
+    const category = event.target.category.value;
+    const image = event.target.image.files[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    // same image to db
+    axios
+      .post(`https://api.imgbb.com/1/upload?key=${imageBbApiKey}`, formData)
+      .then((res) => {
+        if (res.data.success) {
+          const imageUrl = res.data.data.medium.url;
+          axios
+            .post(
+              `${import.meta.env.VITE_server_url}category`,
+              {
+                category,
+                imageUrl,
+              },
+              {
+                headers: {
+                  authorization: `Bearer ${localStorage.getItem("lmt")}`,
+                },
+              }
+            )
+            .then((res) => {
+              // category post done
+              if (res.data.result.acknowledged) {
+                toast.success("Category added successfully!");
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              if (err.response.status === 401 || err.response.status === 403) {
+                logOut()
+                  .then(() => {
+                    toast.error("Session Expired Please login again");
+                    navigate("/login");
+                  })
+                  .catch((err) => {
+                    console.log(err.message);
+                  });
+              }
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.status === 401 || err.response.status === 403) {
+          logOut()
+            .then(() => {
+              toast.error("Session Expired Please login again");
+              navigate("/login");
+            })
+            .catch((err) => {
+              console.log(err.message);
+            });
+        }
+      });
   };
+
+  /*  */
+
   return (
     <div>
       <h1 className="text-4xl font-bold text-start ml-4 mb-4 text-primary">
@@ -17,29 +84,42 @@ const AdminAction = () => {
             Add Category
           </h3>
           <div className="form-control">
-            <form onSubmit={handleAddCategory} className="input-group">
-              <input
-                name="category"
-                type="text"
-                placeholder="add category"
-                className="input input-bordered input-primary"
-              />
-              <button type="submit" className="btn btn-square btn-primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
-              </button>
+            <form
+              onSubmit={handleAddCategory}
+              className="grid grid-cols-1 gap-4"
+            >
+              <div className="form-control mx-auto">
+                <input
+                  name="image"
+                  type="file"
+                  className="file-input file-input-bordered file-input-primary"
+                />
+              </div>
+
+              <div className="input-group">
+                <input
+                  name="category"
+                  type="text"
+                  placeholder="add category"
+                  className="input input-bordered input-primary"
+                />
+                <button type="submit" className="btn btn-square btn-primary">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
+                </button>
+              </div>
             </form>
           </div>
         </div>
