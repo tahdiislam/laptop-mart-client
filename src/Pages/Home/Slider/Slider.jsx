973caafb69c651c18f5f762a50b0f367f0f91b45
tahdi-islam <tshdi60@gmail.com>
2022-12-15 04:30:44 +0600

    customize slider

--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -1,10 +1,8 @@
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import "./Slider.css";
