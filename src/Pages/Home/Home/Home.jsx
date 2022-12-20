25bd81760343410e0a52eb2d3192d6e101ac968d
tahdi-islam <tshdi60@gmail.com>
2022-12-21 02:33:32 +0600

    m

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,40 +1,16 @@
 import React, { useState } from "react";
-import { useEffect } from "react";
 import { Advertise } from "../Advertise/Advertise";
 import Brand from "../Brand/Brand";
 import { Info } from "../Info/Info";
 import Slider from "../Slider/Slider";
-// import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 const Home = () => {
-  // const [loading, setLoading] = useState(false);
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 4000);
-  // }, []);
   return (
     <div>
-      {/* {loading ? (
-        <div style={{height: "100%"}} className="flex w-full justify-center items-center">
-          <ClimbingBoxLoader
-            color={"#4ED01F"}
-            loading={loading}
-            // cssOverride={override}
-            size={30}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        </div>
-      ) : (
-        <div> */}
-          <Slider />
-          <Advertise />
-          <Brand />
-          <Info />
-        {/* </div>
-      )} */}
+      <Slider />
+      <Advertise />
+      <Brand />
+      <Info />
     </div>
   );
 };
