7d9570e43a0cf298460c68a0f552941e3b40b070
tahdi-islam <tshdi60@gmail.com>
2022-12-21 01:12:06 +0600

    add spinner in home page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,42 @@
 import React from "react";
-import {Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer";
 import Header from "../Shared/Header";
+import CircleLoader from "react-spinners/CircleLoader";
 
 export default function Main() {
-  
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <div>
-      <Header/>
-      <Outlet/>
-      <Footer/>
+      {loading ? (
+        <div
+          // style={{ height: "100%" }}
+          className="flex w-full h-screen justify-center items-center"
+        >
+          <CircleLoader
+            color={"#4ED01F"}
+            loading={loading}
+            // cssOverride={override}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
