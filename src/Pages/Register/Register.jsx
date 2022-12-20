531738f4e59e629cff735c0b8727ef6e523f09fc
tahdi-islam <tshdi60@gmail.com>
2022-12-21 01:57:20 +0600

    add loader in login and register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -6,12 +6,17 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../Context/AuthProvider";
 import useAccessToken from "../../Hooks/useAccessToken";
 import RegisterImg from "../../assets/login.png";
+import CircleLoader from "react-spinners/CircleLoader";
 
 const Register = () => {
   const [role, setRole] = useState("buyer");
   const [userEmail, setUserEmail] = useState("");
-  const { createUserWithEmailPassword, updateUserProfile, signInWithProvider } =
-    useContext(UserContext);
+  const {
+    createUserWithEmailPassword,
+    updateUserProfile,
+    signInWithProvider,
+    loading,
+  } = useContext(UserContext);
   const navigate = useNavigate();
 
   // get the access token
@@ -155,126 +160,159 @@ const Register = () => {
   };
 
   return (
-    <section className="w-full px-4 md:px-0">
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content px-0 md:px-4 flex-col lg:flex-row">
-          <div className="w-full md:w-1/2 text-center lg:text-left">
-            <img src={RegisterImg} alt="" />
-          </div>
-          <div className="card w-full md:w-1/2 shadow-2xl bg-base-100">
-            <form onSubmit={handleSubmit(handleLogin)} className="card-body">
-              <h3 className="text-3xl font-semibold text-center">Register</h3>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Full Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="name"
-                  className="input input-bordered input-primary"
-                  {...register("name", { required: "Name is required!" })}
-                />
-                {errors?.name && (
-                  <p className="text-red-500 mt-2">{errors.name.message}</p>
-                )}
+    <section>
+      {loading ? (
+        <div
+          // style={{ height: "100%" }}
+          className="flex w-full h-screen justify-center items-center"
+        >
+          <CircleLoader
+            color={"#4ED01F"}
+            loading={loading}
+            // cssOverride={override}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className="w-full px-4 md:px-0">
+          <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content px-0 md:px-4 flex-col lg:flex-row">
+              <div className="w-full md:w-1/2 text-center lg:text-left">
+                <img src={RegisterImg} alt="" />
               </div>
-              <div className="grid grid-cols-2 gap-6">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Upload a photo</span>
-                  </label>
-                  <input
-                    {...register("image", { required: "Photo is required!" })}
-                    type="file"
-                    className="file-input file-input-bordered file-input-primary w-full"
-                  />
-                  {errors?.image && (
-                    <p className="text-red-500 mt-2">{errors.image.message}</p>
-                  )}
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Select you role</span>
-                  </label>
-                  <select
-                    name="role"
-                    onChange={(event) => setRole((prev) => event.target.value)}
-                    required
-                    className="select select-primary w-full max-w-xs"
-                  >
-                    <option value="buyer" defaultValue="selected">
-                      Buyer
-                    </option>
-                    <option value="seller">Seller</option>
-                  </select>
-                </div>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered input-primary"
-                  {...register("email", { required: "Email is required!" })}
-                />
-                {errors?.email && (
-                  <p className="text-red-500 mt-2">{errors.email.message}</p>
-                )}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered input-primary"
-                  {...register("password", {
-                    required: "Password is required!",
-                    minLength: {
-                      value: 8,
-                      message: "Password must be 8 character or more",
-                    },
-                    pattern: {
-                      value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
-                      message: "Password must be strong",
-                    },
-                  })}
-                />
-                {errors?.password && (
-                  <p className="text-red-500 mt-2">{errors.password.message}</p>
-                )}
-              </div>
-              <div className="form-control mt-6">
-                <button type="submit" className="btn btn-primary">
-                  Register
-                </button>
-              </div>
-              <p className="text-center mt-3">
-                Already have an account?{" "}
-                <Link
-                  className="text-primary underline hover:no-underline"
-                  to="/login"
+              <div className="card w-full md:w-1/2 shadow-2xl bg-base-100">
+                <form
+                  onSubmit={handleSubmit(handleLogin)}
+                  className="card-body"
                 >
-                  Login
-                </Link>{" "}
-                here.
-              </p>
-            </form>
-            <div className="divider mt-[-6px]">OR</div>
-            <div className="flex justify-center mb-4">
-              <button
-                onClick={hanleGoogleSignIn}
-                className="btn btn-ghost w-1/2"
-              >
-                Continue with Google
-              </button>
+                  <h3 className="text-3xl font-semibold text-center">
+                    Register
+                  </h3>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="name"
+                      className="input input-bordered input-primary"
+                      {...register("name", { required: "Name is required!" })}
+                    />
+                    {errors?.name && (
+                      <p className="text-red-500 mt-2">{errors.name.message}</p>
+                    )}
+                  </div>
+                  <div className="grid grid-cols-2 gap-6">
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Upload a photo</span>
+                      </label>
+                      <input
+                        {...register("image", {
+                          required: "Photo is required!",
+                        })}
+                        type="file"
+                        className="file-input file-input-bordered file-input-primary w-full"
+                      />
+                      {errors?.image && (
+                        <p className="text-red-500 mt-2">
+                          {errors.image.message}
+                        </p>
+                      )}
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Select you role</span>
+                      </label>
+                      <select
+                        name="role"
+                        onChange={(event) =>
+                          setRole((prev) => event.target.value)
+                        }
+                        required
+                        className="select select-primary w-full max-w-xs"
+                      >
+                        <option value="buyer" defaultValue="selected">
+                          Buyer
+                        </option>
+                        <option value="seller">Seller</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="email"
+                      className="input input-bordered input-primary"
+                      {...register("email", { required: "Email is required!" })}
+                    />
+                    {errors?.email && (
+                      <p className="text-red-500 mt-2">
+                        {errors.email.message}
+                      </p>
+                    )}
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="password"
+                      className="input input-bordered input-primary"
+                      {...register("password", {
+                        required: "Password is required!",
+                        minLength: {
+                          value: 8,
+                          message: "Password must be 8 character or more",
+                        },
+                        pattern: {
+                          value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                          message: "Password must be strong",
+                        },
+                      })}
+                    />
+                    {errors?.password && (
+                      <p className="text-red-500 mt-2">
+                        {errors.password.message}
+                      </p>
+                    )}
+                  </div>
+                  <div className="form-control mt-6">
+                    <button type="submit" className="btn btn-primary">
+                      Register
+                    </button>
+                  </div>
+                  <p className="text-center mt-3">
+                    Already have an account?{" "}
+                    <Link
+                      className="text-primary underline hover:no-underline"
+                      to="/login"
+                    >
+                      Login
+                    </Link>{" "}
+                    here.
+                  </p>
+                </form>
+                <div className="divider mt-[-6px]">OR</div>
+                <div className="flex justify-center mb-4">
+                  <button
+                    onClick={hanleGoogleSignIn}
+                    className="btn btn-ghost w-1/2"
+                  >
+                    Continue with Google
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
